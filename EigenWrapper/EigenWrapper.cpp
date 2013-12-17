9090d254851940c6211ad24997c05a972fa5c1e6
Ryan Pavlik <rpavlik@iastate.edu>
2013-12-17 16:19:22 -0600

    Split handling of the object into a template c++/cli class (oh noes!)
    Not sure if this clutters things or clarifies, but I think it helps
    by hiding the awkward ownership semantics at the "impedance mismatch"
    between C++ and .NET

--- EigenWrapper/EigenWrapper.cpp
+++ EigenWrapper/EigenWrapper.cpp
@@ -7,25 +7,12 @@
 using namespace EigenWrapper;
 
 MatrixXd::MatrixXd(int x, int y)
-	: matrix_(new Eigen::MatrixXd(x, y))
-	, m_(*matrix_)
+	: m_(new Eigen::MatrixXd(x, y))
 {
 	m_ = Eigen::MatrixXd::Random(x, y);
 }
 
 MatrixXd::MatrixXd(Eigen::MatrixXd const & mat)
-	: matrix_(new Eigen::MatrixXd(mat))
-	, m_(*matrix_)
+	: m_(new Eigen::MatrixXd(mat))
 {
-}
-
-MatrixXd::~MatrixXd()
-{
-	this->!MatrixXd();
-}
-
-MatrixXd::!MatrixXd()
-{
-	delete matrix_;
-	matrix_ = NULL;
 }
--- EigenWrapper/EigenWrapper.h
+++ EigenWrapper/EigenWrapper.h
@@ -6,21 +6,50 @@
 using namespace System;
 
 namespace EigenWrapper {
+	template<typename T>
+	ref class ValuePointerOwner
+	{
+	public:
+		explicit ValuePointerOwner(T * p)
+			: p_(p)
+			, ref_(*p)
+		{}
+
+		ValuePointerOwner(T const & other)
+			: p_(new T(other))
+			, ref_(*p)
+		{}
+
+		// Warning C++ programmers - this is a non-standard operator= because it returns nothing
+		void operator=(T const & other)
+		{
+			if (&other != p_) {
+				ref_ = other;
+			}
+		}
+
+		!ValuePointerOwner() { delete p_; }
+		~ValuePointerOwner() { this->!ValuePointerOwner(); }
+
+		T & get() { assert(p_); return ref_; }
+	private:
+		T * p_;
+		T & ref_;
+	};
 
 	public ref class MatrixXd
 	{
 		// TODO: Add your methods for this class here.
 	public:
+		// Constructor from size
 		MatrixXd(int x, int y);
-		~MatrixXd();
-		!MatrixXd();
+
 		static MatrixXd^ operator *(MatrixXd^ lhs, MatrixXd^ rhs)
 		{
-			return gcnew MatrixXd(lhs->m_ * rhs->m_);
+			return gcnew MatrixXd(lhs->m_.get() * rhs->m_.get());
 		}
 	internal:
-		Eigen::MatrixXd* matrix_;
-		Eigen::MatrixXd & m_;
+		ValuePointerOwner<Eigen::MatrixXd> m_;
 		// Internal constructor for operator results.
 		MatrixXd(Eigen::MatrixXd const & matrix);
 	};
